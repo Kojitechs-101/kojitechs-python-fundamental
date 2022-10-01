116ac9f08b201f68dc740f497bd5e559154073f4
Bkoji1150 <kojibello058@gmail.com>
2022-09-30 17:13:11 -0700

    added types of function

---
+++ Functions/import_function/script1.py
@@ -0,0 +1,26 @@
+
+def addition(x,y):
+    print("-"*88)
+    print(f"The addition of {x} and {y} is: {x + y}")
+    print("-"*88)
+    return None
+
+
+def subtraction(x,y):
+    print("-"*88)
+    print(f"The subtraction of {x} and {y} is: {x - y}")
+    print("-"*88)
+    return None
+
+def main():
+    x = 10
+    y = 45
+    addition(x,y)  
+    subtraction(x,y)
+    return None
+
+if __name__ == "__main__":
+    main()
+
+
+#########
