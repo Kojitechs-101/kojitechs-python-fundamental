c16ddbdb8ced72381f13ca6a978fd908dadb9f9d
Bkoji1150 <kojibello058@gmail.com>
2022-10-02 13:57:31 -0700

    working with OOp's

--- OOPS/main.py
+++ OOPS/main.py
@@ -1,8 +1,85 @@
 # OOP'S
+import os
 
 """
 Object Oriented programming...?
 """
 # Programs are organize in the form of Classes, method, object, arrtibutes
 
-def 
+# def get_details_for_each_python_home(python_home):
+#     path  = python_home 
+#     print(f"python home: {os.path.dirname(python_home)}\npython configuration path: {path} ")
+#     return None
+
+
+# def main():
+#     python_home3 =  "/Library/Frameworks/Python.framework/Versions/3.10/bin"
+#     python_home2 =  "/Library/Frameworks/Python.framework/Versions/2.10/bin"
+#     get_details_for_each_python_home(python_home3)
+#     print("-"*88)
+#     get_details_for_each_python_home(python_home2)
+#     return None
+
+
+# Global variable
+"""
+def get_details_for_each_python_home(python_home):
+    global python_conf_path, ph
+    python_conf_path  = os.path.dirname(python_home)
+    ph = python_home
+    return None
+
+
+def display():   
+    print(f"python home: {os.path.dirname(ph)}\npython configuration path: {python_conf_path} ")
+    return None
+
+
+def main():
+    python_home3 =  "/Library/Frameworks/Python.framework/Versions/3.10/bin"
+    python_home2 =  "/Library/Frameworks/Python.framework/Versions/2.10/bin"
+    get_details_for_each_python_home(python_home3)
+    print("-"*88)
+    get_details_for_each_python_home(python_home2)
+    display()
+    return None
+
+"""    
+
+class Python:
+
+    def get_details_for_each_python_home(self, python_home):
+        self.python_conf_path  = os.path.dirname(python_home)
+        self.ph = python_home
+        return None
+
+
+    def display(self):   
+        print(f"python home: {os.path.dirname(ph)}\npython configuration path: {python_conf_path} ")
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    pass
+    return None
+
+
+
+if __name__ == "__main__":
+    main()    
